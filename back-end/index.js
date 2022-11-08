c71d0141fa64e7d25b0f3ff6c27627afee0a702d
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-11-08 13:27:00 +0530

    Update customer.js

--- back-end/index.js
+++ back-end/index.js
@@ -2,10 +2,12 @@ const express=require('express');
 var cors=require('cors');
 const connection=require('./connection');
 const userRoute=require('./routes/user');
+const customerRoute=require('./routes/customer');
 const app=express();
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use('./user',userRoute);
+app.use('./customer',userRoute);
 
 module.exports=app;
