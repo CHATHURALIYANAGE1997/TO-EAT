71ed4e86e70b781a60a78a8037c6b9c5dd5dcd46
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-11-09 10:18:19 +0530

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, BrowserRouter as Routes, Route } from 'react-router-dom';
 import { Switch as Switch } from 'react-router-dom';
-import Login from './pages/Login'
-
+import Login from './pages/login'
+import signup from './pages/signup'
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
             <Switch>
               <Route path="/" exact component={Login}></Route>
               <Route path="/login" component={Login}></Route>
+              <Route path="/signup" component={signup}></Route>
             </Switch>  
           </div>
         </Routes>
