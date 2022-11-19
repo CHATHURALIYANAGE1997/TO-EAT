211b43506508edfbe98ec66716f3e2ba8c701263
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-11-19 22:01:25 +0530

    Update admin.js

--- back-end/routes/admin.js
+++ back-end/routes/admin.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const connection = require('../connection');
+const router = express.Router();
+const bcrypt = require("bcrypt");
 
+
+const jwt = require('jsonwebtoken');
+const nodemailer=require('nodemailer');
+require('dotenv').config();
+
+
+
+module.exports = router;
