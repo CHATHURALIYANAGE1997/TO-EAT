603980dfdb33bab26d9b0de7ceab3d31ea035506
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-11-22 21:53:00 +0530

    Update authActions.js

--- front-end/src/service/user/auth/authActions.js
+++ front-end/src/service/user/auth/authActions.js
@@ -7,3 +7,11 @@ const AUTH_URL = "http://localhost:8080/user/login";
 export const authenticateUser = (email, password) => async (dispatch) => {
   
 };
+
+export const logoutUser = () => {
+    return (dispatch) => {
+        dispatch(logoutRequest());
+        localStorage.removeItem("jwtToken");
+        dispatch(success({ username: "", isLoggedIn: false }));
+    };
+};
