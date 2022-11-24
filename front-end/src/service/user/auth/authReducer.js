ad44fbc1d4c410dc27cc014dfa9bec91ca2128d0
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-11-24 08:44:53 +0530

    Update authReducer.js

--- front-end/src/service/user/auth/authReducer.js
+++ front-end/src/service/user/auth/authReducer.js
@@ -8,4 +8,25 @@ const initialState = {
     role:""
 };
 
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOGIN_REQUEST:
+        case LOGOUT_REQUEST:
+            return {
+                ...state,
+            };
+        case SUCCESS:
+        case FAILURE:
+            return {
+
+                jwt:action.payload.jwt,
+                username: action.payload.username,
+                isLoggedIn: action.payload.isLoggedIn,
+                role:action.payload.role
+            };
+        default:
+            return state;
+    }
+};
+
 export default reducer;
