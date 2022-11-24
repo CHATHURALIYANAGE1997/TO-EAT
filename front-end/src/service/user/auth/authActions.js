43f6138e05c3967f9d09d5b5ba2f12540c909643
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-11-24 08:43:26 +0530

    Update authActions.js

--- front-end/src/service/user/auth/authActions.js
+++ front-end/src/service/user/auth/authActions.js
@@ -5,7 +5,21 @@ import axios from "axios";
 const AUTH_URL = "http://localhost:8080/user/login";
 
 export const authenticateUser = (email, password) => async (dispatch) => {
-  
+  dispatch(loginRequest());
+    try {
+        const response = await axios.post(AUTH_URL, {
+
+            email: email,
+            password: password,
+        });
+        localStorage.setItem("jwtToken", response.data.token);
+        //console.log(response.data);
+        dispatch(success({ jwt:response.data.token,username: response.data.name, isLoggedIn: true, role: response.data.role, }));
+        return Promise.resolve(response);
+    } catch (error) {
+        dispatch(failure());
+        return Promise.reject(error);
+    }
 };
 
 export const logoutUser = () => {
