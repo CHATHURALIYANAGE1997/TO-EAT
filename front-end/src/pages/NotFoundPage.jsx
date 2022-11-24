a011c2fe260b7e0bb065c977de5cdb9c68511c4c
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-11-24 09:07:46 +0530

    Update NotFoundPage.jsx

--- front-end/src/pages/NotFoundPage.jsx
+++ front-end/src/pages/NotFoundPage.jsx
@@ -1,1 +1,21 @@
+import React, {useEffect, useState} from "react";
+import {useTitle} from "../components/Title/Title";
+import {Link} from "react-router-dom";
+
+const NotFoundPage = (props) => {
+
+        useTitle("404")
+    return(
+
+        <div>
+            <div>
+            <h1 style={{color : "black",textAlign: "center" ,marginTop:"20%"}}>Page Not Found</h1>
+                <h7>You Have to </h7>
+                <Link to={"/"}><span>Login</span></Link>
+                </div>
+        </div>
+    )
+}
+
+export default NotFoundPage;
 
