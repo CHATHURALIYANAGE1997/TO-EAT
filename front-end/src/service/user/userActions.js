4abef7ac6bdea235007f300f4a83b7944a3a1c62
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-11-25 14:58:05 +0530

    Update userActions.js

--- front-end/src/service/user/userActions.js
+++ front-end/src/service/user/userActions.js
@@ -2,13 +2,25 @@ import * as UT from "./userTypes";
 import axios from "axios";
 
 
-const REGISTER_URL = "http://localhost:8080/user/signup";
-
-
 export const registerUser = (userObject) => async (dispatch) => {
+    const REGISTER_URL = "http://localhost:8080/user/signup";
     dispatch(userRequest());
     try {
         const response = await axios.post(REGISTER_URL, userObject);
+        dispatch(userSavedSuccess(response.data));
+        return Promise.resolve(response.data);
+    } catch (error) {
+        dispatch(userFailure(error.message));
+        return Promise.reject(error);
+    }
+};
+
+export const forgotpassword = (userObject) => async (dispatch) => {
+    console.log(userObject);
+    const FORGOT_PASSWORD_URL="http://localhost:8080/user/forgotpassword"
+    dispatch(userRequest());
+    try {
+        const response = await axios.post(FORGOT_PASSWORD_URL, userObject);
         dispatch(userSavedSuccess(response.data));
         return Promise.resolve(response.data);
     } catch (error) {
