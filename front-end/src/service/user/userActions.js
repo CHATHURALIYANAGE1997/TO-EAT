84990dbd245ce373490b1276a65d5f467fc47433
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-11-25 14:54:26 +0530

    Update userActions.js

--- front-end/src/service/user/userActions.js
+++ front-end/src/service/user/userActions.js
@@ -3,3 +3,16 @@ import axios from "axios";
 
 
 const REGISTER_URL = "http://localhost:8080/user/signup";
+
+
+export const registerUser = (userObject) => async (dispatch) => {
+    dispatch(userRequest());
+    try {
+        const response = await axios.post(REGISTER_URL, userObject);
+        dispatch(userSavedSuccess(response.data));
+        return Promise.resolve(response.data);
+    } catch (error) {
+        dispatch(userFailure(error.message));
+        return Promise.reject(error);
+    }
+};
